0ef1e73025dac9651053522cc00058bb74b4d251
Shawna Cooper <scooper1920@gmail.com>
2022-04-21 09:54:06 -0400

    (added time element)

--- MASH.py
+++ MASH.py
@@ -1,5 +1,6 @@
 #This is a game of MANSION, APARTMENT, SHACK, HOUSE
 import random, sys
+import time
 
 love = []
 job =[]
@@ -17,15 +18,19 @@ print ('Please tell me 4 jobs you may want to have.')
 for jobTypes in range (1,5):
     job_answer = input()
     job.append(job_answer)
+    
 print('Your job prospects are ' + ','.join(job) )
 
 print ('I am thinking')
 print('....')
 print('...')
+time.sleep(5)
+
 
 home=['house', 'flat', 'mansion', 'shack']
 
 psychicNumber = random.randint(0,3)
+
 print ('You will marry ' + love[psychicNumber] + '. Your job will be working as a ' + job[psychicNumber] + 
 '. You will live in a ' + home[psychicNumber] + ' and have ' + str(random.randint(1,9)) + ' kid(s)' )
 
